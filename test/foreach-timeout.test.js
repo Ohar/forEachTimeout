6d14b397bfde29e83e80a7cfbe63840879c6553f
Pavel Lysenko <voke.commodus@gmail.com>
2016-08-27 02:02:40 +0300

    Add test

---
+++ test/foreach-timeout.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const chai           = require('chai'),
+      forEachTimeout = require('./../foreach-timeout'),
+      assert         = chai.assert;
+
+describe(
+		'forEachTimeout', () => {
+			
+			it(
+					'Is function',
+					() =>
+							assert.isFunction(forEachTimeout)
+			);
+			
+			it(
+					'Returns Promise',
+					() =>
+							assert.instanceOf(forEachTimeout(), Promise)
+			);
+			
+			it(
+					'Correct result',
+					done => {
+						forEachTimeout([1, 'foo'], e => Promise.resolve(e + ' bar'), 50)
+								.then(
+										results => {
+											assert.equal(
+													JSON.stringify(results),
+													JSON.stringify(['1 bar', 'foo bar'])
+											);
+											done();
+										}
+								);
+					}
+			);
+			
+		}
+);
