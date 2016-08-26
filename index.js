85097410556ff696d6fe96ee87c5b5f099d58334
Pavel lysenko <p.lysenko@zmeke.com>
2016-08-26 19:49:22 +0300

    Add forEachTimeout

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function forEachTimeout (arr, callback, TIMEOUT = 1000) {
+  if (arr && arr.length) {
+    return new Promise(
+      resolve => {
+        const MAX = arr.length - 1;
+
+        let timer    = setTimeout(tick, TIMEOUT),
+            i        = 0,
+            promises = [];
+
+        function tick () {
+          if (i <= MAX) {
+            promises.push(callback(arr[i]));
+            i++;
+            timer = setTimeout(tick, TIMEOUT);
+          } else {
+            clearTimeout(timer);
+            resolve(Promise.all(promises));
+          }
+        }
+      }
+    );
+  } else {
+    return Promise.reject(new Error('Incorrect input array: ', arr));
+  }
+}
+
+module.exports = forEachTimeout;
